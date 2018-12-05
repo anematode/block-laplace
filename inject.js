63865dae0cd4b9f5152f3ea2680ef4d8ec80792b
Timothy Herchen <timothy.herchen@gmail.com>
2018-12-05 12:29:31 -0800

    simplified, less interesting

--- inject.js
+++ inject.js
@@ -47,68 +47,32 @@
                 // note that this script is never run for sites other than schoology.pausd.org, so the other sites are safe.
 
                 try {
-                    // The code in this try block is for rewriting the home page.
+                    setTimeout(function() {
+                        var post_container = document.getElementsByClassName("s-edge-feed")[0];
+                        // Unordered list (ul) containing the posts
 
-                    var resp = JSON.parse(_this.responseText).output;
-                    var respText = _this.responseText;
+                        var posts = Array.prototype.slice.call(
+                            post_container.getElementsByTagName('li') // get all li tags (mostly posts)
+                        ); // convert to Array from HTMLCollection
 
-                    // Convert returned text to HTML object that we can manipulate
-                    var doc = parser.parseFromString(resp, "text/html");
+                        var bad_posts_indexes = [];
 
-                    var post_container = doc.getElementsByClassName("s-edge-feed")[0]; // Unordered list (ul) containing the posts
+                        for (let i = 0; i < posts.length; i++) {
+                            var post = posts[i]; // for each post
 
-                    var posts = Array.prototype.slice.call(
-                        post_container.getElementsByTagName('li') // get all li tags (mostly posts)
-                    ); // convert to Array from HTMLCollection
+                            var links = Array.prototype.slice.call(post.getElementsByTagName("a")); // get all links in the post
+                            for (let j = 0; j < links.length; j++) {
+                                var link = links[j]; // for each link
 
-                    var bad_posts_indexes = [];
+                                if (link.href === FLEX_TIME) { // If the link goes to FlexTime
+                                    post_container.removeChild(post);
+                                    break; // Exit out of the loop, the execution is complete.
+                                }
 
-                    for (let i = 0; i < posts.length; i++) {
-                        var post = posts[i]; // for each post
-
-                        var links = Array.prototype.slice.call(post.getElementsByTagName("a")); // get all links in the post
-                        for (let j = 0; j < links.length; j++) {
-                            var link = links[j]; // for each link
-
-                            if (link.href === FLEX_TIME) { // If the link goes to FlexTime
-                                bad_posts_indexes.push(i);
-                                break; // Exit out of the loop, the execution is complete.
+                                // Note this means that posts not in FlexTime, but contain links to FlexTime, also get removed. Good.
                             }
-
-                            // Note this means that posts not in FlexTime, but contain links to FlexTime, also get removed. Good.
                         }
-                    }
-
-                    var start_li = /\\u003C\s*li([\s]|\\u003E)/gi;
-                    var end_li = /\\u003C\s*\\\s*\/li\s*\\u003E/gi;
-
-                    var start_indices = [];
-                    var end_indices = [];
-
-                    var result;
-                    while ((result = start_li.exec(respText))) {
-                        start_indices.push(result.index);
-                    }
-                    while ((result = end_li.exec(respText))) {
-                        end_indices.push({pos: result.index, length: result[0].length});
-                    }
-
-                    //console.log(start_indices, end_indices, bad_posts_indexes);
-                    //window.egg = respText;
-
-                    for (let i = bad_posts_indexes.length - 1; i >= 0; i--) {
-                        let bpi = bad_posts_indexes[i];
-
-                        let start = start_indices[bpi];
-                        let end = end_indices[bpi];
-
-                        respText = respText.slice(0, start) + respText.slice(end.pos + end.length, respText.length);
-                    }
-
-                    // Override the data!
-                    Object.defineProperty(_this, 'responseText', {
-                        value: respText
-                    });
+                    }, 1000);
                 } catch (e) {console.log(e)} // we don't want to have any annoying errors cause the website to fail!
             }
 
